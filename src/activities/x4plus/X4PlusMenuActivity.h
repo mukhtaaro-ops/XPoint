4003b4b5cce8ce72eaa7014f1a3eccbc6d85f01b
mukhtaaro-ops <mukhtaar.o@gmail.com>
2026-09-19 22:43:41 +0200

    Give X4 Pro+ hub native icon menu structure

--- src/activities/x4plus/X4PlusMenuActivity.h
+++ src/activities/x4plus/X4PlusMenuActivity.h
@@ -1,14 +1,17 @@
 #pragma once
-#include <array>
-#include "activities/UiListActivity.h"
+#include "activities/Activity.h"
+#include "util/ButtonNavigator.h"
 
-class X4PlusMenuActivity final : public UiListActivity {
+class X4PlusMenuActivity final : public Activity {
  public:
   X4PlusMenuActivity(GfxRenderer& renderer, MappedInputManager& mappedInput);
+  void loop() override;
+  void render(RenderLock&&) override;
+
  private:
-  int listCount() const override;
-  void buildScreen(UiScreen& screen) override;
-  void activateIndex(int index) override;
-  const char* headerTitle() const override;
-  std::array<freeink::ui::ListItem, 4> rows{};
+  static constexpr int kItemCount = 4;
+  ButtonNavigator buttonNavigator;
+  int selectedIndex = 0;
+
+  void activateSelection();
 };
