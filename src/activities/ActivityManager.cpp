0cb2f04747b3a2a401e36b82e145aa37acb42033
mukhtaaro-ops <mukhtaar.o@gmail.com>
2026-09-19 22:04:08 +0200

    Fix X4 Pro+ home return target

--- src/activities/ActivityManager.cpp
+++ src/activities/ActivityManager.cpp
@@ -415,7 +415,7 @@ void ActivityManager::goHome(HomeMenuItem initialMenuItem, bool cleanInitialRefr
     } else if (activityName == "OpdsBookBrowser") {
       initialMenuItem = HomeMenuItem::OPDS_BROWSER;
     } else if (activityName == "X4Plus") {
-      initialMenuItem = HomeMenuItem::CALENDAR;
+      initialMenuItem = HomeMenuItem::X4PLUS_TOOLS;
     } else if (activityName == "CrossPointWebServer") {
       initialMenuItem = HomeMenuItem::FILE_TRANSFER;
     } else if (activityName == "Settings") {
