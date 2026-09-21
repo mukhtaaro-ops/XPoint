881b62804f642734dfe3cda9c3600ee2f261d93c
mukhtaaro-ops <mukhtaar.o@gmail.com>
2026-09-21 19:41:18 +0200

    Build 27: add Quran reader and saved ayat entries

--- src/activities/x4plus/X4PlusMenuActivity.cpp
+++ src/activities/x4plus/X4PlusMenuActivity.cpp
@@ -10,7 +10,7 @@
 #include "components/UITheme.h"
 
 namespace {
-constexpr std::array<UIIcon, 6> kIcons = {Recent, Bookmark, Text, Blocks, Bookmark, Book};
+constexpr std::array<UIIcon, 7> kIcons = {Recent, Bookmark, Text, Blocks, Bookmark, Book, Bookmark};
 }
 
 X4PlusMenuActivity::X4PlusMenuActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
@@ -24,6 +24,7 @@ void X4PlusMenuActivity::activateSelection() {
     case 3: activityManager.goToX4PlusCards(); break;
     case 4: activityManager.goToX4PlusStudy(); break;
     case 5: activityManager.goToX4PlusQuran(); break;
+    case 6: activityManager.goToX4PlusQuranNotes(); break;
     default: break;
   }
 }
@@ -80,9 +81,9 @@ void X4PlusMenuActivity::render(RenderLock&&) {
   renderer.clearScreen();
   GUI.drawHeader(renderer, Rect{0, metrics.topPadding, width, metrics.headerHeight}, tr(STR_X4P_TOOLS));
 
-  const std::array<const char*, 6> labels = {
+  const std::array<const char*, 7> labels = {
       tr(STR_X4P_CALENDAR), tr(STR_X4P_TASKS), tr(STR_X4P_NOTES), tr(STR_X4P_CARDS),
-      "Study Cards", "Qur'an"};
+      "Study Cards", "Qur'an Reader", "Saved Ayat / Notes"};
 
   const int menuTop = metrics.topPadding + metrics.headerHeight + metrics.verticalSpacing;
   GUI.drawButtonMenu(renderer,
