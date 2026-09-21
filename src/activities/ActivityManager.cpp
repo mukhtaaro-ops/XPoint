ccd1e9129df84a3198bcde602767ed99ee258598
mukhtaaro-ops <mukhtaar.o@gmail.com>
2026-09-21 21:43:49 +0200

    Build 29: wire 2048 activity

--- src/activities/ActivityManager.cpp
+++ src/activities/ActivityManager.cpp
@@ -363,6 +363,11 @@ void ActivityManager::goToX4PlusMinesweeper() {
   if (!activity) return;
   replaceActivity(std::move(activity));
 }
+void ActivityManager::goToX4Plus2048() {
+  auto activity = makeUniqueNoThrow<X4Plus2048Activity>(renderer, mappedInput);
+  if (!activity) return;
+  replaceActivity(std::move(activity));
+}
 
 void ActivityManager::goToBrowser() {
   const auto& servers = OPDS_STORE.getServers();
