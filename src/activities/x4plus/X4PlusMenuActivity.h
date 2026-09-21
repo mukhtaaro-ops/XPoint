e14c94f03928f81d0f0906cde53b21249841610f
mukhtaaro-ops <mukhtaar.o@gmail.com>
2026-09-21 19:43:25 +0200

    Build 27: add clippings menu slot

--- src/activities/x4plus/X4PlusMenuActivity.h
+++ src/activities/x4plus/X4PlusMenuActivity.h
@@ -9,7 +9,7 @@ class X4PlusMenuActivity final : public Activity {
   void render(RenderLock&&) override;
 
  private:
-  static constexpr int kItemCount = 7;
+  static constexpr int kItemCount = 8;
   ButtonNavigator buttonNavigator;
   int selectedIndex = 0;
 
